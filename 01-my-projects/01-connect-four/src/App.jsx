0dd163ef16b698526731267e74fcf30f222db3e1
develoba <srta.scobar@gmail.com>
2025-02-28 07:40:18 +0100

    feat: grid

--- 01-my-projects/01-connect-four/src/App.jsx
+++ 01-my-projects/01-connect-four/src/App.jsx
@@ -1,8 +1,21 @@
+import { useState } from 'react'
+
 function App () {
+  const [board, setBoard] = useState(Array(25).fill(null))
+
 
   return (
     <>
-      <h2>Connect Four</h2>
+      <section className='board'>
+        <h1>Connect Four</h1>
+        <div className='game'>
+          {board.map((cell, index) => (
+            <div key={index} className='square'>
+              {cell}
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   )
 }
